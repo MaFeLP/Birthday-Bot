6176cd0ac8db68c99a221ec1fd419c666967118c
GitHub <maxfehlinger@gmx.de>
2021-05-31 22:03:27 +0200

    ➕ Added meaningful error messages.

--- src/main/java/com/github/mafelp/commands/WrapCommand.java
+++ src/main/java/com/github/mafelp/commands/WrapCommand.java
@@ -3,13 +3,11 @@ package com.github.mafelp.commands;
 import com.github.mafelp.Builders.PresentBuilder;
 import com.github.mafelp.Listeners.PrivateChannelListener;
 import com.github.mafelp.Main;
-import com.github.mafelp.Manager.PresentManager;
 import com.github.mafelp.utils.Command;
 import com.github.mafelp.utils.CommandParser;
 import com.github.mafelp.utils.Enums.PrivateListenerState;
 import com.github.mafelp.utils.exceptions.CommandNotFinishedException;
 import com.github.mafelp.utils.exceptions.NoCommandGivenException;
-import com.google.gson.JsonObject;
 import com.vdurmont.emoji.EmojiParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -74,31 +72,45 @@ public class WrapCommand extends Thread {
         logger.debug("Executing command wrap...");
 
         if (command.getStringArgument(0).isEmpty()) {
-            logger.debug("Not enough arguments.");
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap\"; Response: Not enough Arguments");
-            sendHelpEmbed();
+            sendHelpEmbed(messageCreateEvent);
             return;
         }
 
         if (messageCreateEvent.getServerTextChannel().isEmpty()) {
+            messageCreateEvent.getChannel().sendMessage(
+                    new EmbedBuilder()
+                    .setAuthor(messageCreateEvent.getMessageAuthor())
+                    .setColor(Color.RED)
+                    .setTitle("Error!")
+                    .setDescription("This command can only be used in ServerTextChannels! Please head over to one, where the bot can read the content as well!")
+            ).join();
+            messageCreateEvent.getMessage().addReaction(EmojiParser.parseToUnicode(":x:"));
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap\"; Response: not a ServerTextChannel.");
             return;
         }
 
         if ("help".equals(command.getStringArgument(0).get().toLowerCase(Locale.ROOT))) {
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap help\"; Response: Help embed");
-            sendHelpEmbed();
+            sendHelpEmbed(messageCreateEvent);
             return;
         }
 
         if (messageCreateEvent.getMessageAuthor().asUser().isEmpty()) {
+            messageCreateEvent.getChannel().sendMessage(
+                    new EmbedBuilder()
+                    .setAuthor(messageCreateEvent.getMessageAuthor())
+                    .setColor(Color.RED)
+                    .setTitle("Error!")
+                    .setDescription("Sorry, but only Users can pack presents! Please be a user to continue.")
+            ).join();
+            messageCreateEvent.getMessage().addReaction(EmojiParser.parseToUnicode(":x:"));
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap\"; Response: Not a user.");
             return;
         }
 
         if (PrivateChannelListener.getListeningState(messageCreateEvent.getMessageAuthor().asUser().get()) == PrivateListenerState.NONE) {
             if (command.getStringArgument(0).get().matches("<@([!]?)([0-123456789]*)>")) {
-                // Removes any '!' from the user, because some user's mention tag don't contain them!
                 StringBuilder builder = new StringBuilder();
                 for (char c : command.getStringArgument(0).get().toCharArray()) {
                     if (c != '<' && c != '@' && c != '!' && c != '>')
@@ -115,98 +127,38 @@ public class WrapCommand extends Thread {
                         return;
                     }
 
-                    PresentBuilder presentBuilder = new PresentBuilder(messageCreateEvent.getMessageAuthor().asUser().get(), receiver, messageCreateEvent.getServer().get(), messageCreateEvent.getMessage());
+                    PresentBuilder presentBuilder = new PresentBuilder(messageCreateEvent.getMessageAuthor().asUser().get(), receiver, messageCreateEvent.getServerTextChannel().get().getServer(), messageCreateEvent.getMessage());
                     presentBuilder.nextStep(command.getStringArgument(0).get());
                 } catch (NumberFormatException e) {
                     logger.debug("ID not a long.");
                 }
             } else {
                 logger.debug("Wrong receiver");
-                messageCreateEvent.addReactionsToMessage(EmojiParser.parseToUnicode(":x:"));
             }
         } else {
-            JsonObject present = new JsonObject();
-
-            for (String completeArgument : command.getArguments()) {
-                StringBuilder argumentBuilder = new StringBuilder();
-                StringBuilder valueBuilder = new StringBuilder();
-                boolean isValue = false;
-
-                for (char c : completeArgument.toCharArray()) {
-                    if (c == '=')
-                        isValue = true;
-                    else {
-                        if (isValue)
-                            valueBuilder.append(c);
-                        else
-                            argumentBuilder.append(c);
-                    }
-                }
-                String firstPartArgument = argumentBuilder.toString();
-                String value = valueBuilder.toString();
-
-                if (firstPartArgument.equals(completeArgument) || value.equals("")) {
-                    //TODO add Argument Error Embed
-                    sendHelpEmbed();
-                    logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap\"; Response: Argument Error. + Help");
-                    return;
-                }
-
-                switch (firstPartArgument.toLowerCase(Locale.ROOT)) {
-                    case "content" -> {
-                        logger.debug("Add to Present: \"content\": \"" + value + "\"");
-                        present.addProperty("content", value);
-                    }
-                    case "imageurl", "imagelink" -> {
-                        logger.debug("Add to Present: \"imageLink\": \"" + value + "\"");
-                        present.addProperty("imageURL", value);
-                    }
-                    case "title" -> {
-                        logger.debug("Add to Present: \"Title\": \"" + value + "\"");
-                        present.addProperty("title", value);
-                    }
-                    case "receiver" -> {
-                        logger.debug("Add to Present: \"receiver\": \"" + value + "\"");
-                        if (value.matches("<@([!]?)([0-123456789]*)>")) {
-                            // Removes any '!' from the user, because some user's mention tag don't contain them!
-                            valueBuilder = new StringBuilder();
-                            for (char c : value.toCharArray()) {
-                                if (c != '!')
-                                    valueBuilder.append(c);
-                            }
-                            value = valueBuilder.toString();
-
-                            present.addProperty("receiver", value);
-                        } else {
-                            logger.debug("Wrong receiver");
-                            messageCreateEvent.addReactionsToMessage(EmojiParser.parseToUnicode(":x:"));
-                            return;
-                        }
-                    }
-                }
-            }
-
-            present.addProperty("author", messageCreateEvent.getMessageAuthor().getId());
-
-            logger.debug("Present is in JSON: " + present);
-            if (messageCreateEvent.getServer().isPresent()) {
-                PresentManager.addPresent(messageCreateEvent.getServer().get(), present);
-                messageCreateEvent.addReactionsToMessage(EmojiParser.parseToUnicode(":white_check_mark:"));
-                PresentManager.savePresents();
-
-                logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap\"; Response: Created Present Embed.");
-            } else {
-                logger.debug("Not on a Server.");
-                messageCreateEvent.addReactionsToMessage(EmojiParser.parseToUnicode(":x:"));
-
-                logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"wrap\"; Response: not on a server.");
-            }
+            messageCreateEvent.getChannel().sendMessage(
+                    new EmbedBuilder()
+                    .setAuthor(messageCreateEvent.getMessageAuthor())
+                    .setColor(Color.RED)
+                    .setTitle("Error!")
+                    .setDescription("Cannot initiate the wizard: You are currently in another wizard! Please cancel or finish this wizard, before executing this command again!")
+            ).join();
+            messageCreateEvent.getMessage().addReaction(EmojiParser.parseToUnicode(":x:")).join();
+            logger.info("User " + messageCreateEvent.getMessageAuthor().asUser().get().getName() + " executed command \"wrap\"; Response: Already in another wizard.");
         }
     }
 
-    private void sendHelpEmbed() {
+    private void sendHelpEmbed(MessageCreateEvent messageCreateEvent) {
         messageCreateEvent.getChannel().sendMessage(
                 new EmbedBuilder()
+                .setColor(new Color(0xFFC270))
+                .setAuthor(messageCreateEvent.getMessageAuthor())
+                .setTitle("Help page for command: " + command.getCommand())
+                .setDescription("The wrap command wraps a present for another server member. This command starts initiating a wizard in a private channel with this bot, where you will be taken through all necessary steps, to create your personal present!")
+                .addField("Arguments","There is only one argument needed to execute this command. This argument is a ping to the member, who shall receiver this present.")
+                .setFooter("Your command message will be deleted, if possible.")
         ).thenAccept(message -> logger.debug("Help message sent!"));
+
+        messageCreateEvent.getMessage().addReaction(EmojiParser.parseToUnicode(":x:"));
     }
 }
