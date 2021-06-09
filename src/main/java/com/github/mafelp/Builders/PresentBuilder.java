6b85aae3b9cfdb5e8367463ec34a345ddb171fd4
GitHub <maxfehlinger@gmx.de>
2021-06-09 22:09:40 +0200

    ➕ Added Javadoc and comments to the PresentBuilder

--- src/main/java/com/github/mafelp/Builders/PresentBuilder.java
+++ src/main/java/com/github/mafelp/Builders/PresentBuilder.java
@@ -22,25 +22,72 @@ import java.util.Map;
 import java.util.concurrent.CompletionException;
 
 public class PresentBuilder {
+    /**
+     * The logger, which is used to log information to the console.
+     */
     private static final Logger logger = LogManager.getLogger(PresentBuilder.class);
 
+    /**
+     * The map which saves the current state and all the presentBuilds for Users.
+     */
     private static final Map<User, PresentBuilder> presentBuilders = new HashMap<>();
 
+    /**
+     * The state of the current builder.
+     */
     private PresentBuilderState state;
+    /**
+     * The present which will be configured.
+     */
     private final JsonObject present;
+
+    /**
+     * The channel, to which the messages were sent.
+     */
     private PrivateChannel channel;
+
+    /**
+     * The server on which the presents should be created.
+     */
     private final Server server;
+
+    /**
+     * The user who created the present.
+     */
     private final User sender;
+
+    /**
+     * The user who shall receive and can unpack the present.
+     */
     private final User receiver;
+
+    /**
+     * The first message sent by this bot in order to initiate the wizard.
+     */
     private Message message;
+
+    /**
+     * The Listener, which listens to to {@link PresentBuilder#message}.
+     */
     private final PresentBuilderReactionListener presentBuilderReactionListener = new PresentBuilderReactionListener();
 
+    /**
+     * @param sender The user who has triggered this event and will create a present.
+     * @param receiver The first argument, which defines the user, who shall receive and be able to unpack the present.
+     * @param server The server on which the present shall be unpacked.
+     * @param originalMessage The original message, sent on the server.
+     */
     public PresentBuilder(User sender, User receiver, Server server, Message originalMessage) {
+        // Initiates some builder resources.
         this.state = PresentBuilderState.START;
         this.sender = sender;
         this.receiver = receiver;
         this.server = server;
+        this.present = new JsonObject();
+        present.addProperty("sender", sender.getId());
+        present.addProperty("receiver", receiver.getId());
 
+        // Gets/opens a private channel with the user.
         if (sender.getPrivateChannel().isPresent())
             this.channel = sender.getPrivateChannel().get();
         else {
@@ -57,29 +104,42 @@ public class PresentBuilder {
             originalMessage.delete().thenAccept(none -> logger.debug("Deleted original Message which contained the wrap command."));
         else
             originalMessage.addReaction(EmojiParser.parseToUnicode(":white_check_mark:")).thenAccept(none -> logger.debug("Could not delete original Message. Added a reaction instead."));
-
-        this.present = new JsonObject();
-        present.addProperty("sender", sender.getId());
-        present.addProperty("receiver", receiver.getId());
-
         presentBuilders.put(sender, this);
     }
 
+    /**
+     * @param user The user whose present builder will be returned.
+     * @return The present builder from the User.
+     */
     public static PresentBuilder getPresentBuilder(@NotNull User user) {
         return presentBuilders.get(user);
     }
 
+    /**
+     * @return Getter for the JSONObject, which is the present.
+     */
     public JsonObject getPresent() {
         return this.present;
     }
 
+    /**
+     * @param builderState The current state in the enum that represents the current state of the present builder.
+     * @return The new state of the present builder.
+     */
     public PresentBuilder setState(PresentBuilderState builderState) {
         this.state = builderState;
         return this;
     }
 
+    /**
+     * This method sends a preview to the configured private channel.
+     */
     public void sendPreview() {
+        if (channel == null)
+            return;
+
         JsonObject previewPresent = present.getAsJsonObject();
+        // Logic to prevent the PresentManager#buildPresent from failing with an error, that no title/content was found.
         if (previewPresent.get("title") == null || previewPresent.get("title").getAsString().isEmpty() || previewPresent.get("title").getAsString().isBlank()) {
             previewPresent.addProperty("title","Placeholder-Title");
             logger.debug("Adding temporary title to present: " + present.get("title").getAsString());
@@ -94,6 +154,11 @@ public class PresentBuilder {
         ).thenAccept(message -> logger.debug("Sent Preview Message"));
     }
 
+    /**
+     * The method that handles the actual execution of the present builder.
+     * @param content The content of the message.
+     * @return The current instance of the present builder to allow the chaining of methods.
+     */
     public PresentBuilder nextStep(String content) {
         if (channel == null) {
             return null;
@@ -101,6 +166,7 @@ public class PresentBuilder {
 
         switch (this.state) {
             case START -> {
+                // Initiation sequence, will be executed on startup and sends the Wizard-Start-Message.
                 if (!(PrivateChannelListener.setListeningState(this.sender, PrivateListenerState.PRESENT))) {
                     logger.debug("There is currently another listening in this channel is progress!");
                     return null;
@@ -116,6 +182,8 @@ public class PresentBuilder {
                         .addField("Reactions","To get a preview of the current present, just react to this message with " + EmojiParser.parseToUnicode(":eye:") + "\n\nTo build the present, just react to this message with " + EmojiParser.parseToUnicode(":white_check_mark:") + "\nTo cancel the wizard process, just react to this message with " + EmojiParser.parseToUnicode(":x:"))
                         .addField("Step 1","As a first Step, Please give a short title for this present.")
                 ).join();
+
+                // Also add some reactions, to make the life of the user easier.
                 message.addReaction(EmojiParser.parseToUnicode(":eye:"));
                 message.addReaction(EmojiParser.parseToUnicode(":x:"));
                 message.addReactionAddListener(this.presentBuilderReactionListener);
@@ -123,6 +191,7 @@ public class PresentBuilder {
                 this.state = PresentBuilderState.TITLE;
             }
             case TITLE -> {
+                // The first step of the present builder, which sets the title of the present.
                 present.addProperty("title", content);
                 channel.sendMessage(
                         new EmbedBuilder()
@@ -135,6 +204,7 @@ public class PresentBuilder {
                 this.state = PresentBuilderState.CONTENT;
             }
             case CONTENT -> {
+                // The second step of the present builder, which sets the content of the present.
                 present.addProperty("content", content);
                 message.addReaction(EmojiParser.parseToUnicode(":white_check_mark:"));
                 channel.sendMessage(
@@ -149,6 +219,7 @@ public class PresentBuilder {
                 this.state = PresentBuilderState.IMAGE;
             }
             case IMAGE -> {
+                // The third step of the present builder, which sets the image of the present.
                 present.addProperty("imageURL", content);
                 channel.sendMessage(
                         new EmbedBuilder()
@@ -164,7 +235,7 @@ public class PresentBuilder {
                 return this.nextStep(content);
             }
             case FINISHED -> {
-                // Cleanup routine.
+                // Cleanup routine, will be executed after the last step.
                 channel.sendMessage("Your finished Present:",
                         PresentManager.buildPresent(this.present)
                 ).thenAccept(message -> logger.debug("Successfully built the present!"));
@@ -176,7 +247,7 @@ public class PresentBuilder {
                 PresentManager.savePresents(this.server);
             }
             case CANCELLED -> {
-                // Cancel routine
+                // Cancel routine, will be executed, if the user has reacted with :x:
                 channel.sendMessage(
                         new EmbedBuilder()
                         .setAuthor(this.sender)
