98beb3404ebba895259e5840fae5835fe3620916
GitHub <maxfehlinger@gmx.de>
2021-06-11 22:30:25 +0200

    📗 Added Javadoc to the PersonCommand

--- src/main/java/com/github/mafelp/commands/PersonCommand.java
+++ src/main/java/com/github/mafelp/commands/PersonCommand.java
@@ -9,14 +9,40 @@ import java.awt.*;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The class that handles the asynchronous execution of the person command.
+ */
 public class PersonCommand extends Thread {
+    /**
+     * The number of threads of this kind that were being created.
+     */
     private static long threadID = 0;
+
+    /**
+     * The Event that is being passed to this class by the discord API.
+     */
     private final MessageCreateEvent messageCreateEvent;
+
+    /**
+     * The list of member ids, specified in the configuration of the server, on which the message was sent on.
+     */
     private final List<Long> members;
 
+    /**
+     * The instance to select a random game.
+     */
     private static final Random random = new Random();
+
+    /**
+     * The logger which is used to log statements to the console.
+     */
     private static final Logger logger = LogManager.getLogger(PersonCommand.class);
 
+    /**
+     * The "Default" Constructor
+     * @param messageCreateEvent The Event that is being passed to this class by the discord API.
+     * @param members The list of members specified in the configuration of the server, on which the message was sent.
+     */
     public PersonCommand(MessageCreateEvent messageCreateEvent, List<Long> members) {
         this.messageCreateEvent = messageCreateEvent;
         this.members = members;
@@ -25,6 +51,9 @@ public class PersonCommand extends Thread {
         ++threadID;
     }
 
+    /**
+     * The method handles the actual execution of this command.
+     */
     @Override
     public void run() {
         logger.debug("Executing command person...");
