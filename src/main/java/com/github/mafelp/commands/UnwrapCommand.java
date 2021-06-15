ee66176526840568cfe7cec224c2c59813afc00f
GitHub <maxfehlinger@gmx.de>
2021-06-15 13:00:46 +0200

    🛠 Tweaked the regex which checks if the argument is a valid user id. 📗 Added javadoc and comments to the Unwrap command.

--- src/main/java/com/github/mafelp/commands/UnwrapCommand.java
+++ src/main/java/com/github/mafelp/commands/UnwrapCommand.java
@@ -15,17 +15,39 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import java.awt.*;
 import java.util.Arrays;
 
+/**
+ * The class that handles asynchronous execution of the Unwrap command.
+ */
 public class UnwrapCommand extends Thread {
+    /**
+     * The logger which is used to log statements to the console.
+     */
+    private static final Logger logger = LogManager.getLogger(SkribblCommand.class);
+
+    /**
+     * The number of threads of this kind that were being created.
+     */
     private static long threadID = 0;
 
+
+    /**
+     * The Event that is being passed to this class by the discord API.
+     */
     private final MessageCreateEvent messageCreateEvent;
-    private Command command;
 
-    private static final Logger logger = LogManager.getLogger(UnwrapCommand.class);
+    /**
+     * The command which was being parsed with the {@link com.github.mafelp.utils.CommandParser} command parser.
+     */
+    private final Command command;
 
+    /**
+     * Default Constructor sets thread names for this thread.
+     * @param messageCreateEvent The Event that is being passed to this class by the discord API.
+     */
     public UnwrapCommand(MessageCreateEvent messageCreateEvent) {
         this.messageCreateEvent = messageCreateEvent;
 
+	// Initialises the command with the message content, not the <b>readable</b> message contents.
         Command cmd = null;
         try {
             cmd = CommandParser.parseFromString(messageCreateEvent.getMessageContent());
@@ -53,37 +75,43 @@ public class UnwrapCommand extends Thread {
                             .addField("Command not finished Exception", "Please finish your command with a quotation mark!")
             );
         }
-
         this.command = cmd;
 
         this.setName("UnwrapCommand-" + threadID);
         ++threadID;
     }
 
+    /**
+     * The method handles the actual execution of this command.
+     */
     @Override
     public void run() {
         if (this.command == null)
             return;
 
+	// Check if arguments were given. If not, send the help hembed
         if (command.getStringArgument(0).isEmpty()) {
             sendHelpMessage(true);
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"unwrap\"; Response: Not enough arguments.");
             return;
         }
 
+	// Check if the first argument is a valid ping to a user.
         String receiverTag = command.getStringArgument(0).get();
-        if (!receiverTag.matches("<@([!]?)([0-123456789]*)>")) {
+        if (!receiverTag.matches("<@([!]?)[1-9]{1}[0-9]{16,18}>")) {
             sendHelpMessage(true);
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"unwrap\"; Response: Wrong argument.");
             return;
         }
 
+	// Check if the first argument is "help", if so, send the help message.
         if (receiverTag.equalsIgnoreCase("help")) {
             sendHelpMessage(false);
             logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" executed command \"unwrap help\"; Help page.");
             return;
         }
 
+	// Check if the message was sent on a server
         if (messageCreateEvent.getServer().isEmpty()) {
             messageCreateEvent.getChannel().sendMessage(
                     new EmbedBuilder()
@@ -95,6 +123,7 @@ public class UnwrapCommand extends Thread {
             return;
         }
 
+	// check if the message creator is a user.
         if (messageCreateEvent.getMessageAuthor().asUser().isEmpty()) {
             messageCreateEvent.getChannel().sendMessage(
                     new EmbedBuilder()
@@ -107,8 +136,10 @@ public class UnwrapCommand extends Thread {
             return;
         }
 
+	// Get all presents for the user on this server.
         var presents = PresentManager.getPresents(messageCreateEvent.getServer().get(), messageCreateEvent.getMessageAuthor().asUser().get());
 
+	// If this user does not have any presents, send an error.
         if (presents == null) {
             messageCreateEvent.getChannel().sendMessage(
                     new EmbedBuilder()
@@ -121,12 +152,17 @@ public class UnwrapCommand extends Thread {
             return;
         }
 
+	// Build all presents and ssend them to the channel.
         for (JsonObject jsonObject : presents) {
             logger.debug("Current present: " + jsonObject.toString());
             messageCreateEvent.getChannel().sendMessage(PresentManager.buildPresent(jsonObject)).thenAccept(message -> logger.info("User \"" + messageCreateEvent.getMessageAuthor().getName() + "\" unpacked present \"" + jsonObject.get("title").getAsString() + "\"."));
         }
     }
 
+    /**
+     * Sends the help message to the channel, in which the command was executed.
+     * @param beError if the message should have the error as a title and the color red (only help embed does not send an error).
+     */
     private void sendHelpMessage(boolean beError) {
         EmbedBuilder embed = new EmbedBuilder()
                 .setColor(new Color(0xFFC270))
