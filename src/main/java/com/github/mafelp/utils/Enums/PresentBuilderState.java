5fad5bdbc677cdd34febbe2caba069d955845544
GitHub <maxfehlinger@gmx.de>
2021-06-17 12:10:24 +0200

    📗 Added javadoc to the Enums

--- src/main/java/com/github/mafelp/utils/Enums/PresentBuilderState.java
+++ src/main/java/com/github/mafelp/utils/Enums/PresentBuilderState.java
@@ -1,10 +1,31 @@
 package com.github.mafelp.utils.Enums;
 
+/**
+ * The State the present builder can be in.
+ */
 public enum PresentBuilderState{
+    /**
+     * Start indicates it is initialising the wizard.
+     */
     START,
+    /**
+     * It indicates that it is waiting for input on the title.
+     */
     TITLE,
+    /**
+     * It indicates that it is waiting for input on the content.
+     */
     CONTENT,
+    /**
+     * It indicates that it is waiting for input on an image link.
+     */
     IMAGE,
+    /**
+     * It indicates that it is finished and now cleaning up.
+     */
     FINISHED,
+    /**
+     * It indicates that the user cancelled the wizard and it is now cleaning up.
+     */
     CANCELLED,
 }
--- src/main/java/com/github/mafelp/utils/Enums/PrivateListenerState.java
+++ src/main/java/com/github/mafelp/utils/Enums/PrivateListenerState.java
@@ -1,6 +1,15 @@
 package com.github.mafelp.utils.Enums;
 
+/**
+ * The Wizards a user can have running in its private chat.
+ */
 public enum PrivateListenerState {
+    /**
+     * None indicates there is no wizard running and the user can start one.
+     */
     NONE,
+    /**
+     * Present indicates that there is a {@link com.github.mafelp.Builders.PresentBuilder} wizard running.
+     */
     PRESENT,
 }
